5d55b635a23988f26e3a378829fbb797db20e422
thauane <thauane.carvalho@fatec.sp.gov.br>
2022-10-27 18:23:26 -0300

    apostila01(oracle_cloud): bloco de codigo 2.10.1 exibindo apenas nome, sem interaçao sobre colecao

--- src/PessoaService.java
+++ src/PessoaService.java
@@ -2,6 +2,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import lombok.RequiredArgsConstructor;
 import java.net.URI;
 
@@ -17,7 +21,13 @@ public class PessoaService {
                 .uri(URI.create(url)).
                 build();
            var response = client.send(request, BodyHandlers.ofString());
-           System.out.println(response.body());
+           JSONObject raiz = new JSONObject(response.body());
+           JSONArray items = raiz.getJSONArray("items");
+           System.out.println(items);
+           JSONObject primeiro = items.getJSONObject(0);
+           System.out.println(primeiro);
+           String  nome = primeiro.getString("nome");
+           System.out.println(nome);
         }
         catch(Exception e){
             System.out.println(e.getMessage());
